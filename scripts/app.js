3ee0b0221386347810977da8bd0d12cdf0947652
Eric Todd <39493826+EricMTodd@users.noreply.github.com>
2022-10-25 14:43:15 -0600

    MVP achieved, preparing to deploy.

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+console.log('app.js is running...')
+const postsUrl = 'https://jsonplaceholder.typicode.com/posts'
+const usersUrl = 'https://jsonplaceholder.typicode.com/users'
+
+fetch(usersUrl)
+.then(response => response.json())
+.then(users => {
+  console.log('users: ', users)
+  users.map(user => {
+    let table = document.querySelector('table')
+    let tableHeader = document.createElement('th')
+    tableHeader.id = user.id
+    tableHeader.className = 'username'
+    tableHeader.innerText = user.username
+    tableHeader.addEventListener('click', () => {
+      console.log(user.username)
+      fetch(postsUrl)
+      .then(response => response.json())
+      .then(posts => {
+        let userPosts = posts.filter(post => post.userId === user.id)
+        console.log('userPosts: ', userPosts)
+        let orderedList = document.querySelector('ol')
+        orderedList.innerText = ''
+        userPosts.map(post => {
+          let listItem = document.createElement('li')
+          let title = document.createElement('h2')
+          title.innerText = post.title
+          let body = document.createElement('p')
+          body.innerText = post.body
+          listItem.appendChild(title)
+          listItem.appendChild(body)
+          orderedList.appendChild(listItem)
+        })
+      })
+      .catch(error => console.log(error))
+    })
+    table.appendChild(tableHeader)
+  })
+})
+.catch(error => console.log(error))
